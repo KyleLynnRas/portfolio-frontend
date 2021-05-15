89659e3c8e2f1686e03750bc9331d150606d9595
KyleLynnRas <kylelynn.ras@gmail.com>
2021-05-15 02:47:32 -0400

    Update footer/header

--- src/components/FooterComp.js
+++ src/components/FooterComp.js
@@ -5,8 +5,8 @@ import { Footer } from "react-bulma-components"
 const FooterComp = (props) => {
     return(
         <Footer className="footer-container">
-             <img src="https://i.imgur.com/d8rUKWq.png" alt="technology icons" className="footer-img" />
-             <p>2021</p>
+            <img src="https://i.imgur.com/d8rUKWq.png" alt="technology icons" className="footer-img" />
+            <p>2021</p>
         </Footer>
     ) 
 }
